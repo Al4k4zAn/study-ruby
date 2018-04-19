73d1bd450e1833d963349d97f92ef3a5d1aa2066
Luis Henrique Chagas Leite <luishenriquech@gmail.com>
2018-04-18 22:35:51 -0300

    Add files hello and types from ruby

---
+++ src/hello.rb
@@ -0,0 +1,7 @@
+#puts 'Olá Ruby!'  
+
+puts 'Qual é o seu nome?'
+nome = STDIN.gets
+
+#puts 'Olá ' + nome
+puts "Olá #{nome}!"
---
+++ src/tipos.rb
@@ -0,0 +1,9 @@
+produto = 'Notebook Gamer'
+preço = 3000
+quantidade = 10
+disponivel = true
+
+puts produto
+puts preço 
+puts quantidade
+puts disponivel
