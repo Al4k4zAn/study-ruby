3609d5f073d28dbde0ba21d05f4e948a4184822f
Luis Henrique Chagas Leite <luishenriquech@gmail.com>
2018-05-16 22:37:58 -0300

    Auto stash before rebase of "origin/master"

--- src/testlink.rb
+++ src/testlink.rb
@@ -8,12 +8,29 @@ def adiciona_ct
 end
 
 def lista_cts
-    puts 'Listando os casos de testes:'
+    puts 'Listando os cenários de testes:'
     @cts.each do |ct|
         puts ct
     end
 end
 
-adiciona_ct
-lista_cts
-puts @cts[0]
+@opcao = 0
+
+begin
+    puts '##### Tesk Link RB #####'
+    puts '1 - Adicionar um Cenário'
+    puts '2 - Listar Cenário'
+    puts '3 - Sair'
+    puts '########################'
+
+    @opcao = STDIN.gets.to_i
+
+    case @opcao
+    when 1
+        acidiona_ct
+    when 2
+        lista_cts
+    end 
+        puts 'Opcoes invalidas'
+end while @opcao != 3
+        
